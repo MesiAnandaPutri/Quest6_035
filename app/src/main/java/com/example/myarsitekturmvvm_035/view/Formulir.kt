76af1ea189d51fe9b4bad6083206e819fbd38db1
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-21 17:06:58 +0700

    Radio button pilihan gender

--- app/src/main/java/com/example/myarsitekturmvvm_035/view/Formulir.kt
+++ app/src/main/java/com/example/myarsitekturmvvm_035/view/Formulir.kt
@@ -78,3 +78,29 @@ fun FormSiswa(
                     .padding(all = 20.dp)
                     .width(width = 250.dp), thickness = Thickness, color = Color.Red
             )
+            Row {
+                pilihanJK.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment = Alignment.CenterVertically)
+                    {
+                        RadioButton(
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
