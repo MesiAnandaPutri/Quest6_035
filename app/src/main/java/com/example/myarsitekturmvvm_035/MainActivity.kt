9cc5f5e1ae99b9d841bc883d9e89adf5afe84d37
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-21 17:16:10 +0700

    menjalankan aplikasi

--- app/src/main/java/com/example/myarsitekturmvvm_035/MainActivity.kt
+++ app/src/main/java/com/example/myarsitekturmvvm_035/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myarsitekturmvvm_035.ui.theme.MyArsitekturMVVM_035Theme
+import com.example.myarsitekturmvvm_035.view.uicontroller.DataSiswa
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +20,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyArsitekturMVVM_035Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    DataSiswa()
                 }
             }
         }
