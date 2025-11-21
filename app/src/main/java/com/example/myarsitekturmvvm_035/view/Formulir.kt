09c5c80b73cbfacdac35686f32c5a9b6d56878e7
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-21 17:03:40 +0700

    list data yang akan dikirim saat tombol submit ditekan

--- app/src/main/java/com/example/myarsitekturmvvm_035/view/Formulir.kt
+++ app/src/main/java/com/example/myarsitekturmvvm_035/view/Formulir.kt
@@ -44,3 +44,7 @@ fun FormSiswa(
     var txtNama by rememberSaveable { mutableStateOf("")}
     var txtAlamat by remember { mutableStateOf("") }
     var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
