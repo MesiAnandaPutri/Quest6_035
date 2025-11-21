af561724a735c0f87b2861b2cc7200d7165f8658
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-21 16:53:12 +0700

    menyiapkan state data siswa menggunakan mutablesstatedlow

---
+++ app/src/main/java/com/example/myarsitekturmvvm_035/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.myarsitekturmvvm_035.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.myarsitekturmvvm_035.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
