a3f4a6423cf928e7ea2cd601877259b6c8df6e31
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-21 16:49:09 +0700

    membuat data jenis kelamin

---
+++ app/src/main/java/com/example/myarsitekturmvvm_035/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.myarsitekturmvvm_035.model
+
+import com.example.myarsitekturmvvm_035.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
