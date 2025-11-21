6819e877351980caa2091d7afa64407329114b55
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-21 17:10:22 +0700

    mengambil state data siswa secara realtime dari viewmodel menggunakan state flow

---
+++ app/src/main/java/com/example/myarsitekturmvvm_035/view/uicontroller/Navigasi.kt
@@ -0,0 +1,31 @@
+package com.example.myarsitekturmvvm_035.view.uicontroller
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myarsitekturmvvm_035.model.DataJK.JenisK
+import com.example.myarsitekturmvvm_035.view.FormSiswa
+import com.example.myarsitekturmvvm_035.view.TampilSiswa
+import com.example.myarsitekturmvvm_035.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataSiswa(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang->
+        val uiState = viewModel.statusUI.collectAsState()
